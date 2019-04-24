6edfcbf3afbbb81f04c80af869e2a2bce352eb56
shipof123 <34876781+shipof123@users.noreply.github.com>
2019-04-24 12:57:55 -0400

    Update sv.cpp

--- sv.cpp
+++ sv.cpp
@@ -1,21 +1,26 @@
-#include <stringstream>
+#include <sstream>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-
-struct SV<char delim> {
+template <char delim>
+struct SV {
   SV(std::string filename) {
     std::ifstream in(filename);
-    
+    std::cout << "<table>\n";
     std::string line, value;
     while(getline(in, line)) {
       auto linestream = std::istringstream(line);
-      std::cout << "<hr>\n|";
+      std::cout << "<tr>\t";
       while(getline(linestream, value, delim)) {
-        std::cout << value << " | "
+        std::cout << "<td>" << value << "</td>\t";
       }
-      std::cout << "<hr>" << std::endl;
+      std::cout << "</tr>\n";
     }
+    std::cout << "</table>" << std::endl;
   }
+};
+
+int main(){
+    auto tsv = SV<' '>("new.tsv");
 }
